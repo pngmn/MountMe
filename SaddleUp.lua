e37af177f77fb2e470b0ff08ee01d3593e4a4187
Phanx <addons@phanx.net>
2014-11-09 16:41:02 +0000

    EVERYTHING

--- SaddleUp.lua
+++ SaddleUp.lua
@@ -1,164 +1,135 @@
--- Tiefer! Tiefer!
+------------------------------------------------------------------------
 
-local MOD_DISMOUNT_FLYING = "shift"
 local MOD_TRAVEL_FORM = "ctrl"
+local MOD_DISMOUNT_FLYING = "shift"
 
 ------------------------------------------------------------------------
 
-local button = CreateFrame("Button", "PhanxMountButton", nil, "SecureActionButtonTemplate")
-button:RegisterForClicks("AnyUp")
+local mountItems = {
+	[37011] = "/use %s %s", -- Magic Broom
+}
+
+local DISMOUNT = [[
+/leavevehicle [canexitvehicle]
+/dismount [mounted]
+/cancelform [form]
+]]
+
+local SAFE_DISMOUNT = "/stopmacro [flying,nomod:%s]"
+
+local button = CreateFrame("Button", "AnyFavoriteMountButton", nil, "SecureActionButtonTemplate")
 button:SetAttribute("type", "macro")
 
-local _, class = UnitClass("player")
-local MOUNT_LINE, MOUNT_CONDITION, MACRO_LINES = 1
-
-local MACRO_TEMPLATE = {
-	format('/stopmacro [flying,nomod:%s]', MOD_DISMOUNT_FLYING),
-	'/changeactionbar [form] 1',
-	'/cancelform [form]',
-	'/dismount [mounted]',
-	'/leavevehicle [canexitvehicle]',
-}
-
-------------------------------------------------------------------------
-if class == "DRUID" then
-	local SPELL_TRAVEL_FORM = GetSpellInfo(783)
-	MOUNT_CONDITION = format('[nocombat,noform,nomounted,noswimming,outdoors,nomod:%s]', MOD_TRAVEL_FORM)
-	MACRO_LINES = {
-		format('/run if SecureCmdOptionParse("%s") and not UnitInVehicle("player") and not IsPlayerMoving() then C_MountJournal.Summon(0) end', MOUNT_CONDITION),
-		format('/cast [noform,nomounted] %s', SPELL_TRAVEL_FORM),
-	}
-
-------------------------------------------------------------------------
-elseif class == "SHAMAN" then
-	local SPELL_GHOST_WOLF = GetSpellInfo(2645)
-	MOUNT_CONDITION = format('[noform,nomounted,nocombat,outdoors,nomod:%s]', MOD_TRAVEL_FORM)
-	MACRO_LINES = {
-		format('/run if SecureCmdOptionParse("%s") and not UnitInVehicle("player") and not IsPlayerMoving() then C_MountJournal.Summon(0) end', MOUNT_CONDITION),
-		format('/cast [noform,nomounted] %s', SPELL_GHOST_WOLF),
-	}
-
-------------------------------------------------------------------------
-else
-	MOUNT_CONDITION = '[nomounted,nocombat,outdoors]'
-	MACRO_LINES = {
-		format('/run if SecureCmdOptionParse("%s") and not UnitInVehicle("player") and not IsPlayerMoving() then C_MountJournal.Summon(0) end', MOUNT_CONDITION),
-	}
+local function GetMountCondition()
+	return("[outdoors,nocombat,nomounted,novehicleui]")
 end
+
+local function GetMount()
+	return format("/run if not IsPlayerMoving() and not UnitInVehicle('player') and SecureCmdOptionParse('%s') then C_MountJournal.Summon(0) end", GetMountCondition())
+end
+
+local GetTravelForm
+
 ------------------------------------------------------------------------
 
-local specials = {
-	function()
-		-- Vashj'ir Seahorse
-		for i = 1, C_MountJournal.GetNumMounts() do
-			local creatureName, spellID, _, _, isUsable = C_MountJournal.GetMountInfo(i)
-			if spellID == 75207 and isUsable and IsSubmerged() then
-				return format('/run if SecureCmdOptionParse("%s") and not UnitInVehicle("player") and not IsPlayerMoving() then C_MountJournal.Summon(%d) end', MOUNT_CONDITION, i)
+if select(2, UnitClass("player")) == "DRUID" then
+
+	local flyingSpell = {
+		[0]   = 90267,  -- Flight Master's License / Eastern Kingdoms
+		[1]   = 90267,  -- Flight Master's License / Kalimdor
+		[646] = 90267,  -- Flight Master's License / Deepholm
+		[571] = 54197,  -- Cold Weather Flying / Northrend
+		[870] = 115913, -- Wisdom of the Four Winds / Pandaria
+	}
+
+	local function CanFly() -- because IsFlyableArea is a fucking liar
+		if IsFlyableArea() and (IsSpellKnown(34090) or IsSpellKnown(34091) or IsSpellKnown(90265)) then
+			local _, _, _, _, _, _, _, instanceMapID = GetInstanceInfo()
+			local reqSpell = flyingSpell[instanceMapID]
+			if not reqSpell or IsSpellKnown(reqSpell) then
+				return true
 			end
 		end
-	end,
-	function()
-		-- Magic Broom @ Hallow's End
-		local count, name = GetItemCount(37011), GetItemInfo(37011)
-		if name and count > 0 then
-			return format("/use %s %s", MOUNT_CONDITION, name)
-		end
-	end,
-}
+	end
 
-button:RegisterEvent("PLAYER_LOGIN")
+	local CAT_FORM_ID, TRAVEL_FORM_ID = 768, 783
+	local CAT_FORM, TRAVEL_FORM = GetSpellInfo(CAT_ID), GetSpellInfo(TRAVEL_ID)
+
+	function GetMountCondition()
+		return format("[outdoors,nocombat,nomounted,noform,novehicleui,nomod:%s]", MOD_TRAVEL_FORM)
+	end
+
+	function GetMount()
+		local str = format("/run if not IsPlayerMoving() and not UnitInVehicle('player') and SecureCmdOptionParse('%s') then C_MountJournal.Summon(0) end", GetMountCondition())
+		if CanFly() then
+			return format("/cast [outdoors,nocombat,nomounted,noform,novehicleui,nomod:%s] %s\n%s", MOD_TRAVEL_FORM, TRAVEL_FORM, str)
+		end
+		return str
+	end
+
+	function GetTravelForm()
+		if IsPlayerSpell(TRAVEL_FORM_ID) then
+			return format("/cast [outdoors] [swimming] %s\n/cast %s", TRAVEL_FORM, CAT_FORM)
+		elseif IsPlayerSpell(TRAVEL_FORM_ID) then
+			return format("/cast %s", CA_FORMT)
+		end
+	end
+
+------------------------------------------------------------------------
+
+elseif select(2, UnitClass("player")) == "SHAMAN" then
+
+	local GHOST_WOLF_ID = 2645
+	local GHOST_WOLF = GetSpellInfo(GHOST_WOLF_ID)
+
+	function GetMountCondition()
+		return format("[outdoors,nocombat,nomounted,noform,novehicleui,nomod:%s]", MOD_TRAVEL_FORM)
+	end
+
+	function GetMount()
+		return format("/run if not IsPlayerMoving() and not UnitInVehicle('player') and SecureCmdOptionParse('%s') then C_MountJournal.Summon(0) end", GetMountCondition())
+	end
+
+	function GetTravelForm()
+		if IsPlayerSpell(GHOST_WOLF_ID) then
+			return format("/cast %s", GHOST_WOLF)
+		end
+	end
+
+end
+
+------------------------------------------------------------------------
+
+function button:Update(event)
+	print(event)
+	if not InCombatLockdown() then
+		local useMount
+		local useForm = GetTravelForm and GetTravelForm()
+		local safetyCheck = not GetCVarBool("autoDismountFlying") and format(SAFE_DISMOUNT, MOD_DISMOUNT_FLYING)
+		for id, line in pairs(mountItems) do
+			if GetItemCount(id) > 0 then
+				useMount = format(line, GetMountCondition(), GetItemInfo(id))
+				break
+			end
+		end
+		if not useMount then
+			useMount = GetMount()
+		end
+		self:SetAttribute("macrotext", strtrim(strjoin("\n", useMount or "", useForm or "", safetyCheck or "", DISMOUNT)))
+
+		ClearOverrideBindings(self)
+		local a, b = GetBindingKey("DISMOUNT")
+		if a then
+			SetOverrideBinding(self, false, a, "CLICK AnyFavoriteMountButton:LeftButton")
+		end
+		if b then
+			SetOverrideBinding(self, false, b, "CLICK AnyFavoriteMountButton:LeftButton")
+		end
+		SetOverrideBinding(self, false, "CTRL-`", "CLICK AnyFavoriteMountButton:LeftButton")
+	end
+end
+
+button:SetScript("OnEvent", button.Update)
 button:RegisterEvent("PLAYER_ENTERING_WORLD")
 button:RegisterEvent("PLAYER_REGEN_ENABLED")
-button:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-button:SetScript("OnEvent", function(self, event)
-	if InCombatLockdown() then return end
-
-	local MOUNT = MACRO_LINES[MOUNT_LINE]
-	for i = 1, #specials do
-		local special = specials[i]()
-		if special then
-			MOUNT = special
-			break
-		end
-	end
-
-	local macro = ""
-	for i = 1, #MACRO_LINES do
-		local line = MACRO_LINES[i]
-		if i == MOUNT_LINE then
-			macro = macro .. "\n" .. MOUNT
-		else
-			macro = macro .. "\n" .. line
-		end
-	end
-	for i = 1, #MACRO_TEMPLATE do
-		macro = macro .. "\n" .. MACRO_TEMPLATE[i]
-	end
-	macro = strsub(macro, 2)
-
-	self:SetAttribute("macrotext", macro)
-end)
-
-------------------------------------------------------------------------
--- Allow setting class mounts as favorites
-
-local isFake = {}
-local shouldFake = {}
-
-local GetIsFavorite = C_MountJournal.GetIsFavorite
-local SetIsFavorite = C_MountJournal.SetIsFavorite
-local GetMountInfo  = C_MountJournal.GetMountInfo
-local Summon        = C_MountJournal.Summon
-
-local f = CreateFrame("Frame")
-f:RegisterEvent("PLAYER_ENTERING_WORLD")
-f:SetScript("OnEvent", function()
-	for index = 1, C_MountJournal.GetNumMounts() do
-		local _, canFavorite = GetIsFavorite(index)
-		if not canFavorite then
-			shouldFake = shouldFake or {}
-			shouldFake[index] = true
-		end
-	end
-end)
-
-function C_MountJournal.GetIsFavorite(index)
-	if shouldFake[index] then
-		print("GetIsFavorite", (GetMountInfo(index)), index, isFake[index])
-		return not not isFake[index], true -- return false instead of nil
-	end
-	return isFavorite, true
-end
-
-function C_MountJournal.SetIsFavorite(index, value)
-	print("SetIsFavorite", (GetMountInfo(index)), index, value)
-	if shouldFake[index] then
-		isFake[index] = value or nil -- remove instead of setting to false
-	else
-		SetIsFavorite(index, value)
-	end
-end
-
-function C_MountJournal.GetMountInfo(index)
-	local creatureName, spellID, icon, active, isUsable, sourceType, isFavorite, isFactionSpecific, faction, _, isCollected = GetMountInfo(index)
-	if shouldFake[index] then
-		return creatureName, spellID, icon, active, isUsable, sourceType, isFake[index], isFactionSpecific, faction, _, isCollected
-	end
-	return creatureName, spellID, icon, active, isUsable, sourceType, isFavorite, isFactionSpecific, faction, _, isCollected
-end
-
-local randoms = {}
-
-function C_MountJournal.Summon(index)
-	if index == 0 and IsMounted() and next(isFake) then
-		wipe(randoms)
-		for j = 1, C_MountJournal.GetNumMounts() do
-			local _, _, _, _, isUsable, _, isFavorite = C_MountJournal.GetMountInfo(j)
-			if isUsable and isFavorite then
-				tinsert(randoms, j)
-			end
-		end
-		index = randoms[random(num)]
-	end
-	Summon(index)
-end
+button:RegisterEvent("UPDATE_BINDINGS")
