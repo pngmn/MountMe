781cc0d1ee5ef8a4b6b5354f51005ac9bd7dd881
Andrew Mordecai <armordecai@protonmail.com>
2017-06-20 23:30:40 -0700

    Fix the Nagrand garrison mount

--- Addon.lua
+++ Addon.lua
@@ -6,8 +6,6 @@
 ------------------------------------------------------------------------
 	TODO:
 	- Cancel transformation buffs that block mounting?
-	- Handle shaman Glyph of Ghostly Speed (GW = ground mount OOC)
-	- Verify that monk Roll works works when morphed into Chi Torpedo talent
 	- Ignore garrison stables training mounts
 ----------------------------------------------------------------------]]
 
@@ -34,24 +32,22 @@ local GetItemCount, GetSpellInfo, HasDraenorZoneAbility, IsOutdoors, IsPlayerMov
 
 ------------------------------------------------------------------------
 
-local GARRISON_ABILITY = GetSpellInfo(161691)
 local ACTION_MOUNT = "/click MountJournalSummonRandomFavoriteButton" -- "/run C_MountJournal.SummonByID(0)"
 local ACTION_MOUNT_PATTERN = gsub(ACTION_MOUNT, "[%(%)]", "%%%1")
 
-local castWhileMovingBuffs = {
-	[GetSpellInfo(172106) or ""] = true, -- Aspect of the Fox
-	[GetSpellInfo(108839) or ""] = PLAYER_CLASS == "MAGE"    or nil, -- Ice Floes
-	[GetSpellInfo(79206)  or ""] = PLAYER_CLASS == "SHAMAN"  or nil, -- Spiritwalker's Grace
-	[GetSpellInfo(137587) or ""] = PLAYER_CLASS == "WARLOCK" or nil, -- Kil'jaeden's Cunning
+local SpellID = {
+	["Cat Form"] = 768,
+	["Darkflight"] = 68992,
+	["Garrison Ability"] = 161691,
+	["Ghost Wolf"] = 2645,
+	["Flight Form"] = 165962,
+	["Summon Mechashredder 5000"] = 164050,
+	["Travel Form"] = 783,
 }
 
-local function IsMoving()
-	for buff in next, castWhileMovingBuffs do
-		if UnitBuff("player", buff) then
-			return false
-		end
-	end
-	return IsPlayerMoving()
+local SpellName = {}
+for k, v in pairs(SpellID) do
+	SpellName[k] = GetSpellInfo(v)
 end
 
 local function HasRidingSkill(flyingOnly)
@@ -80,9 +76,6 @@ button:SetAttribute("type", "macro")
 
 ------------------------------------------------------------------------
 if PLAYER_CLASS == "DRUID" then
-
-	local CAT_FORM_ID, TRAVEL_FORM_ID, FLIGHT_FORM_ID = 768, 783, 165962
-	local CAT_FORM, TRAVEL_FORM, FLIGHT_FORM = GetSpellInfo(CAT_FORM_ID), GetSpellInfo(TRAVEL_FORM_ID), GetSpellInfo(FLIGHT_FORM_ID)
 	local STAG_GLYPH, TRAVEL_GLYPH = 164, 1127
 
 	local BLOCKING_FORMS
@@ -112,65 +105,75 @@ if PLAYER_CLASS == "DRUID" then
 		end
 
 		local mountOK, flightOK = SecureCmdOptionParse(MOUNT_CONDITION), ns.CanFly()
-		if mountOK and flightOK and IsPlayerSpell(FLIGHT_FORM_ID) then
-			return "/cast " .. FLIGHT_FORM
-		elseif mountOK and flightOK and IsPlayerSpell(TRAVEL_FORM_ID) then
+		if mountOK and flightOK and IsPlayerSpell(SpellID["Travel Form"]) then
 			return "/cast " .. TRAVEL_FORM
-		elseif mountOK and not IsMoving() and HasRidingSkill() then
+		elseif mountOK and not IsPlayerMoving() and HasRidingSkill() then
 			return ACTION_MOUNT
-		elseif IsPlayerSpell(TRAVEL_FORM_ID) and (IsOutdoors() or IsSubmerged()) then
-			return "/cast [nomounted,noform] " .. TRAVEL_FORM
-		elseif IsPlayerSpell(CAT_FORM_ID) then
-			return "/cast [nomounted,noform" .. BLOCKING_FORMS .. "] " .. CAT_FORM
+		elseif IsPlayerSpell(SpellID["Travel Form"]) and (IsOutdoors() or IsSubmerged()) then
+			return "/cast [nomounted,noform] " .. SpellName["Travel Form"]
+		elseif IsPlayerSpell(SpellID["Cat Form"]) then
+			return "/cast [nomounted,noform" .. BLOCKING_FORMS .. "] " .. SpellName["Cat Form"]
 		end
 	end
 
 ------------------------------------------------------------------------
 elseif PLAYER_CLASS == "SHAMAN" then
 
-	local GHOST_WOLF_ID = 2645
-	local GHOST_WOLF = GetSpellInfo(GHOST_WOLF_ID)
-
 	MOUNT_CONDITION = "[outdoors,nocombat,nomounted,noform,novehicleui,nomod:" .. MOD_TRAVEL_FORM .. "]"
 	DISMOUNT = DISMOUNT .. "\n/cancelform [form]"
 
 	function GetAction()
-		if not IsMoving() and HasRidingSkill() and SecureCmdOptionParse(MOUNT_CONDITION) then
+		if not IsPlayerMoving() and HasRidingSkill() and SecureCmdOptionParse(MOUNT_CONDITION) then
 			return ACTION_MOUNT
-		elseif IsPlayerSpell(GHOST_WOLF_ID) then
-			return "/cast [nomounted,noform] " .. GHOST_WOLF
+		elseif IsPlayerSpell(SpellID["Ghost Wolf"]) then
+			return "/cast [nomounted,noform] " .. SpellName["Ghost Wolf"]
 		end
 	end
 
 ------------------------------------------------------------------------
 else
-	local movingActionID
-	=  PLAYER_CLASS == "DEATHKNIGHT" and 96268  -- Death's Advance
-	or PLAYER_CLASS == "HUNTER"      and 5118   -- Aspect of the Cheetah
-	or PLAYER_CLASS == "MAGE"        and 108843 -- Blazing Speed
-	or PLAYER_CLASS == "MONK"        and 109132 -- Roll
-	or PLAYER_CLASS == "PALADIN"     and 85599  -- Speed of Light
-	or PLAYER_CLASS == "ROGUE"       and 2983   -- Sprint
-	or PLAYER_CLASS == "WARLOCK"     and 111400 -- Burning Rush
-
-	local movingAction = movingActionID and GetSpellInfo(movingActionID)
+	local ClassActionIDs = {
+		196555, -- Demon Hunter: Netherwalk (1.5m)
+		125883, -- Monk: Zen Flight
+		115008, -- Monk: Chi Torpedo (2x 20s)
+		109132, -- Monk: Roll (2x 20s)
+		190784, -- Paladin: Divine Speed (45s)
+		202273, -- Paladin: Seal of Light
+		  2983, -- Rogue: Sprint (1m)
+		111400, -- Warlock: Burning Rush
+		 68992, -- Worgen: Darkflight (2m)
+	}
+	local ClassActionLimited = {
+		[125883] = function(combat) return combat or IsIndoors() end, -- Zen Flight
+	}
 
 	function GetAction()
-		local moving = IsMoving()
-		if movingAction and IsPlayerSpell(movingActionID) and (moving or UnitAffectingCombat("player")) then
-			return "/cast [nomounted,novehicleui] " .. movingAction
+		local combat = UnitAffectingCombat("player")
+
+		local classAction
+		for i = 1, #ClassActionIDs do
+			local id = ClassActionIDs[i]
+			if IsPlayerSpell(id) and not (ClassActionLimited[id] and ClassActionLimited[id](combat)) then
+				classAction = GetSpellInfo(id)
+				break
+			end
+		end
+
+		local moving = IsPlayerMoving()
+		if classAction and (moving or combat) then
+			return "/cast [nomounted,novehicleui] " .. classAction
 		elseif not moving then
 			local action
 			if HasRidingSkill() and SecureCmdOptionParse(MOUNT_CONDITION) then
 				action = ACTION_MOUNT
 			end
-			if PLAYER_CLASS == "WARLOCK" then
-				action = "/cancelaura " .. movingAction .. (action and ("\n/cancelform [form]\n" .. action) or "")
+			if classAction and PLAYER_CLASS == "WARLOCK" then
+				-- TODO: why is /cancelform in here???
+				action = "/cancelaura " .. classAction .. (action and ("\n/cancelform [form]\n" .. action) or "")
 			end
 			return action
 		end
 	end
-
 end
 
 ------------------------------------------------------------------------
@@ -178,13 +181,12 @@ end
 local GetMountAction
 do
 	local GetMountInfoByID = C_MountJournal.GetMountInfoByID
-	local SUMMON = "/run C_MountJournal.SummonByID(%d)"
 	local SEA_LEGS = GetSpellInfo(73701)
 
 	local SEA_TURTLE = 312
 	local VASHJIR_SEAHORSE = 373
 	local SUBDUED_SEAHORSE = 420
-	local CHAFFEUR = UnitFactionGroup("player") == "Horde" and 679 or 678
+	local CHAUFFEUR = UnitFactionGroup("player") == "Horde" and 679 or 678
 
 	local AQBUGS = {
 		117, -- Blue Qiraji Battle Tank
@@ -198,26 +200,28 @@ do
 	function GetMountAction()
 		-- Magic Broom
 		-- Instant but not usable in combat
-		if IsMoving() then
-			return GetItemCount(37011) > 0 and "/use " .. GetItemInfo(37011)
+		if IsPlayerMoving() and GetItemCount(37011) > 0 then
+			return "/use " .. GetItemInfo(37011)
 		end
 
 		-- Nagrand garrison mounts: Frostwolf War Wolf, Telaari Talbuk
 		-- Can be summoned in combat
-		local name, _, _, _, _, _, id = GetSpellInfo(GARRISON_ABILITY)
-		if (id == 164222 or id == 165803) and HasDraenorZoneAbility() and SecureCmdOptionParse(GARRISON_MOUNT_CONDITION) and (UnitAffectingCombat("player") or not ns.CanFly()) then
-			return "/use " .. name
+		if GetZoneAbilitySpellInfo() == SpellID["Garrison Ability"] then
+			local _, _, _, _, _, _, id = GetSpellInfo(SpellName["Garrison Ability"])
+			if (id == 164222 or id == 165803) and SecureCmdOptionParse(GARRISON_MOUNT_CONDITION) and (UnitAffectingCombat("player") or not ns.CanFly()) then
+				return "/cast " .. SpellName["Garrison Ability"]
+			end
 		end
 
-		if not not SecureCmdOptionParse(MOUNT_CONDITION..",nomod:"..MOD_TRAVEL_FORM) then
+		if not not SecureCmdOptionParse(MOUNT_CONDITION .. ",nomod:" .. MOD_TRAVEL_FORM) then
 			return
 		end
 
-		-- Use Chaffeured Chopper if no riding skill
+		-- Use Chauffeured Chopper if no riding skill
 		if not HasRidingSkill() then
-			local _, _, _, _, chaffeur = GetMountInfoByID(CHAFFEUR)
-			if chaffeur then
-				return format(SUMMON, CHAFFEUR)
+			local _, _, _, _, chauffeur = GetMountInfoByID(CHAUFFEUR)
+			if chauffeur then
+				return "/cast " .. CHAUFFEUR
 			else
 				return
 			end
@@ -226,19 +230,19 @@ do
 		-- Use underwater mounts while swimming
 		if IsSubmerged() then
 			-- Vashj'ir Seahorse (550% swim speed in Vashj'ir)
-			local _, _, _, _, seahorse = GetMountInfoByID(VASHJIR_SEAHORSE)
-			if seahorse then return format(SUMMON, VASHJIR_SEAHORSE) end
+			local seahorseName, _, _, _, seahorseUsable = GetMountInfoByID(VASHJIR_SEAHORSE)
+			if seahorseUsable then return "/cast " .. seahorseName end
 
 			-- Subdued Seahorse (400% swim speed in Vashj'ir, 160% swim speed elsewhere)
-			_, _, _, _, seahorse = GetMountInfoByID(SUBDUED_SEAHORSE)
-			if seahorse and UnitBuff("player", SEA_LEGS) then return format(SUMMON, SUBDUED_SEAHORSE) end
+			seahorseName, _, _, _, seahorseUsable = GetMountInfoByID(SUBDUED_SEAHORSE)
+			if seahorseUsable and UnitBuff("player", SEA_LEGS) then return "/cast " .. seahorseName end
 
 			-- Sea Turtle (160% swim speed)
-			local _, _, _, _, turtle = GetMountInfoByID(SEA_TURTLE)
-			if turtle and seahorse then
-				return format(SUMMON, math.random(1, 2) == 1 and SEA_TURTLE or SUBDUED_SEAHORSE)
+			local turtleName, _, _, _, turtleUsable = GetMountInfoByID(SEA_TURTLE)
+			if turtleUsable and seahorseUsable then
+				return "/cast " .. (math.random(1, 2) == 1 and turtleName or seahorseName)
 			elseif turtle then
-				return format(SUMMON, SEA_TURTLE)
+				return "/cast " .. turtleName
 			end
 		end
 
@@ -256,11 +260,11 @@ do
 						onlyFavorites = true
 					end
 					numBugs = numBugs + 1
-					hasBugs[numBugs] = bug
+					hasBugs[numBugs] = name
 				end
 			end
 			if numBugs > 0 then
-				return format(SUMMON, hasBugs[math.random(numBugs)])
+				return "/cast " .. hasBugs[math.random(numBugs)]
 			end
 		end
 	end
