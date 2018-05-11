de4347f830006d029c0a2ed63ed1511531237eae
Phanx <addons@phanx.net>
2018-05-10 21:12:09 -0700

    Localize options text

--- Favorites.lua
+++ Favorites.lua
@@ -5,11 +5,11 @@
 	https://github.com/phanx-wow/MountMe
 ----------------------------------------------------------------------]]
 
-local f = CreateFrame("Frame")
-f:SetScript("OnEvent", function(f, e, ...) return f[e](f, ...) end)
-f:RegisterEvent("PLAYER_LOGIN")
+local Favorites = CreateFrame("Frame")
+Favorites:SetScript("OnEvent", function(f, e, ...) return f[e](f, ...) end)
+Favorites:RegisterEvent("PLAYER_LOGIN")
 
-function f:PLAYER_LOGIN()
+function Favorites:PLAYER_LOGIN()
 	MountMeSettings = MountMeSettings or {}
 
 	if MountMeSettings.favoritesPerChar then
@@ -24,13 +24,13 @@ function f:PLAYER_LOGIN()
 	end
 end
 
-function f:MOUNT_JOURNAL_SEARCH_UPDATED()
+function Favorites:MOUNT_JOURNAL_SEARCH_UPDATED()
 	if self.settingFavorites then
 		self:SetFavoriteMounts(MountMeFavorites)
 	end
 end
 
-function f:GetFavoriteMounts()
+function Favorites:GetFavoriteMounts()
 	local favorites = {}
 	for i = 1, C_MountJournal.GetNumDisplayedMounts() do
 		local _, _, _, _, _, _, isFavorite, _, _, _, _, mountID = C_MountJournal.GetDisplayedMountInfo(i)
@@ -42,7 +42,7 @@ function f:GetFavoriteMounts()
 	return favorites
 end
 
-function f:SetFavoriteMounts(favorites)
+function Favorites:SetFavoriteMounts(favorites)
 	self.settingFavorites = true
 	self:RegisterEvent("MOUNT_JOURNAL_SEARCH_UPDATED")
 	MountJournal:UnregisterEvent("MOUNT_JOURNAL_SEARCH_UPDATED")
@@ -64,8 +64,8 @@ function f:SetFavoriteMounts(favorites)
 	end
 end
 
-function f.SetIsFavorite(index, isFavorite)
-	local self = f
+function Favorites.SetIsFavorite(index, isFavorite)
+	local self = Favorites
 	if self.settingFavorites or not self.active then return end
 
 	-- By the time this post-hook is running, the indices have already changed,
@@ -73,7 +73,6 @@ function f.SetIsFavorite(index, isFavorite)
 	-- added or removed as a favorite. We'll just get the new list and compare.
 
 	local favorites = self:GetFavoriteMounts()
-	local action = isFavorite and "Add" or "Remove"
 	local a = isFavorite and favorites or MountMeFavorites
 	local b = isFavorite and MountMeFavorites or favorites
 
@@ -85,35 +84,72 @@ function f.SetIsFavorite(index, isFavorite)
 	end
 end
 
+function Favorites:Enable()
+	MountMeFavorites = self:GetFavoriteMounts()
+	if not self.hooked then
+		hooksecurefunc(C_MountJournal, "SetIsFavorite", self.SetIsFavorite)
+		self.hooked = true
+	end
+	self.active = true
+end
+
+function Favorites:Disable()
+	self.active = false
+end
+
+------------------------------------------------------------------------
+
+local L = {
+	AVAILABLE_COMMANDS = "Available commands:",
+	OFF = "OFF",
+	ON = "ON",
+	PER_CHAR_DESC = "Save favorite mounts per character",
+	PER_CHAR_OFF = "Now saving favorite mounts account-wide.",
+	PER_CHAR_ON = "Now saving favorite mounts per character.",
+	VERSION_INFO_S = "Version %s loaded.",
+}
+
+if GetLocale() == "deDE" then
+	L.AVAILABLE_COMMANDS = "Verfügbare Befehle:"
+	L.OFF = "AUS"
+	L.ON = "AN"
+	L.PER_CHAR_DESC = "Speichern Lieblingsreittiere pro Charakter"
+	L.PER_CHAR_OFF = "Lieblingsreittiere wird nun kontoweit gespeichert."
+	L.PER_CHAR_ON = "Lieblingsreittiere wird nun pro Charakter gespeichert."
+	L.VERSION_INFO_S = "Version %s geladen."
+elseif GetLocale():match("^es") then
+	L.AVAILABLE_COMMANDS = "Comandos disponibles:"
+	L.OFF = "INACTIVO"
+	L.ON = "ACTIVO"
+	L.PER_CHAR_DESC = "Guardar monturas favoritas por personaje"
+	L.PER_CHAR_OFF = "Monturas favoritas ahora se guardan para toda la cuenta."
+	L.PER_CHAR_ON = "Monturas favoritas ahora se guardan por personaje."
+	L.VERSION_INFO_S = "Versión %s cargada."
+end
+
 SLASH_MOUNTME1 = "/mountme"
 SlashCmdList["MOUNTME"] = function(cmd)
-	local self = f
 	cmd = (cmd or ""):lower()
 
 	if cmd == "char" then
 		local v = not MountMeSettings.favoritesPerChar
 		MountMeSettings.favoritesPerChar = v
 		if v then
-			MountMeFavorites = self:GetFavoriteMounts()
-			if not self.hooked then
-				hooksecurefunc(C_MountJournal, "SetIsFavorite", self.SetIsFavorite)
-				self.hooked = true
-			end
-			self.active = true
+			Favorites:Enable()
 			DEFAULT_CHAT_FRAME:AddMessage(string.format("%sMountMe:|r %s", NORMAL_FONT_COLOR_CODE,
-				"Now saving favorite mounts per-character."))
+				L.PER_CHAR_ON))
 		else
-			self.active = false
+			Favorites:Disable()
 			DEFAULT_CHAT_FRAME:AddMessage(string.format("%sMountMe:|r %s", NORMAL_FONT_COLOR_CODE,
-				"Now saving favorite mounts account-wide."))
+				L.PER_CHAR_OFF))
 		end
 	return end
 
-	DEFAULT_CHAT_FRAME:AddMessage(string.format("%sMountMe:|r Version %s loaded. %s", NORMAL_FONT_COLOR_CODE,
-		GetAddOnMetadata("MountMe", "Version"),
-		"Available commands:"))
+	DEFAULT_CHAT_FRAME:AddMessage(string.format("%sMountMe:|r %s %s", NORMAL_FONT_COLOR_CODE,
+		string.format(L.VERSION_INFO_S, GetAddOnMetadata("MountMe", "Version")),
+		L.AVAILABLE_COMMANDS))
 	DEFAULT_CHAT_FRAME:AddMessage(string.format("- %s%s|r - %s (%s%s|r)", NORMAL_FONT_COLOR_CODE,
-		"char", "Toggle saving favorite mounts per-character",
+		"char", L.PER_CHAR_DESC,
 		MountMeSettings.favoritesPerChar and GREEN_FONT_COLOR_CODE or GRAY_FONT_COLOR_CODE,
-		MountMeSettings.favoritesPerChar and "ON" or "OFF"))
+		MountMeSettings.favoritesPerChar and L.ON or L.OFF))
 end
